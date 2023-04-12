f6c569bfe76c32942a3dcd53b28f9eb00eb342bc
victorialange <viclan2009@hotmail.com>
2023-04-12 07:20:57 -0400

    Create recipe search bar widget to allow users to search for recipes

---
+++ lib/views/widgets/recipe_search_bar.dart
@@ -0,0 +1,33 @@
+// this will be the search bar shown in the home screen
+import 'package:flutter/material.dart';
+
+class RecipeSearchBar extends StatelessWidget {
+  final void Function(String query) onSearch;
+
+  const RecipeSearchBar({required this.onSearch, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      // text field for user inpute
+      child: TextField(
+        decoration: InputDecoration(
+          // placeholder text to tell user what to do
+          hintText: "Search recipes...",
+          border: const OutlineInputBorder(),
+          suffixIcon: IconButton(
+            // search icon
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              FocusScope.of(context).unfocus();
+              onSearch.call("");
+            },
+          ),
+        ),
+        // upon submit, i.e. user presses submit button from keyboard, onSubmitted gets assigned to onSearch method initialized in the recipe search bar class that takes a string argument (query) from user input, gets assigned anonymous function in home screen widget where recipes state gets updated and fetch method gets called with user input from search bar)
+        onSubmitted: onSearch,
+      ),
+    );
+  }
+}
