4fb420d414669b2d58d0e2dcd265d66688752e41
victorialange <viclan2009@hotmail.com>
2023-04-12 07:00:02 -0400

    Wrap card inside gesture detector to redirect user to details screen upon tap

--- lib/views/widgets/recipe_card.dart
+++ lib/views/widgets/recipe_card.dart
@@ -1,153 +1,79 @@
-// custom recipe widget - this will contain the layout for the recipe card
-
+// custom recipe widget - this will contain the layout for the recipe card used in the home screen
 import 'package:flutter/material.dart';
+// import recipe model to display data
+import 'package:recipe_app/models/recipe.dart';
 // import cached network image package to display images from a URL (can cache and load images from the network, so reduced number of network requests and able to store images in a local cache for increased speed in retrieving data and better performance)
 import 'package:cached_network_image/cached_network_image.dart';
+// import recipe details screen widget to use it as redirect in gesture detector
+import 'package:recipe_app/views/recipe_details_screen.dart';
 
-// define class for recipe card storing the initial data
 class RecipeCard extends StatelessWidget {
-  // setting the variables
-  final String title;
-  final String calories;
-  final String cookTime;
-  // this will hold the image url
-  final String thumbnailUrl;
+  // access data from recipe model
+  final Recipe recipe;
 
-  const RecipeCard({
-    super.key,
-    required this.title,
-    required this.calories,
-    required this.cookTime,
-    required this.thumbnailUrl,
-  });
+  const RecipeCard({super.key, required this.recipe});
 
-  // build method with layout
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
-      // set the width to the width of the device using MediaQuery
-      width: MediaQuery.of(context).size.width,
-      height: 180,
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(15),
-        // create a box shadow for thumbnail image
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.6),
-            offset: const Offset(
-              0.0,
-              10.0,
-            ),
-            blurRadius: 10.0,
-            spreadRadius: -6.0,
+    // wrap card inside gesture detector to redirect user to recipe details screen upon card tap
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          // redirect to recipeDetailsScreen widget
+          MaterialPageRoute(
+            builder: (_) => RecipeDetailsScreen(recipe: recipe),
           ),
-        ],
-        // thumbnail image
-        image: DecorationImage(
-          colorFilter: ColorFilter.mode(
-            Colors.black.withOpacity(0.35),
-            BlendMode.multiply,
-          ),
-          image: NetworkImage(thumbnailUrl),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Stack(
-        children: [
-          // align recipe title to center of card
-          Align(
-            alignment: Alignment.center,
-            // use Padding to center title
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 5.0),
-              // recipe title
-              child: Text(
-                title,
-                style: const TextStyle(
-                  fontSize: 19,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+        );
+      },
+      // recipe card
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // image inside rectangle
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: CachedNetworkImage(
+                  imageUrl: recipe.image,
+                  width: double.infinity,
+                  height: 150,
+                  fit: BoxFit.cover,
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
                 ),
-                // handle text overflow with ellipsis
-                overflow: TextOverflow.ellipsis,
-                // restrict text to 2 lines
-                maxLines: 2,
-                // align text to center
-                textAlign: TextAlign.center,
               ),
-            ),
+              // space between image and text
+              const SizedBox(height: 8),
+              // recipe title
+              Text(
+                recipe.label,
+                style: Theme.of(context).textTheme.titleMedium,
+                maxLines: 1,
+                // in case of overflow account for it with ellipsis(3 dots)
+                overflow: TextOverflow.ellipsis,
+              ),
+              // space between recipe title and source
+              const SizedBox(height: 8),
+              // have source and heart icon appear on same line
+              Row(
+                // spread source and icon apart
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // recipe source
+                  Text(
+                    "by ${recipe.source}",
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  // favourite heart icon
+                  const Icon(Icons.favorite_border, size: 16),
+                ],
+              ),
+            ],
           ),
-          // align cook time and calories to bottom left
-          Align(
-            alignment: Alignment.bottomLeft,
-            // horizontally align cook time and calories
-            child: Row(
-              // separate cook time and calories with space between
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // calories container
-                Container(
-                  padding: const EdgeInsets.all(5),
-                  margin: const EdgeInsets.all(10),
-                  // box decoration for rounded edges
-                  decoration: BoxDecoration(
-                    color: Colors.black.withOpacity(0.4),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  // calories
-                  child: Row(
-                    children: [
-                      const Icon(
-                        Icons.star,
-                        color: Colors.yellow,
-                        size: 18,
-                      ),
-                      const SizedBox(width: 7),
-                      Text(
-                        calories,
-                        style: const TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                // cook time container
-                Container(
-                  padding: const EdgeInsets.all(5),
-                  margin: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.black.withOpacity(0.4),
-                    // round edges of cook time container
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  // cook time
-                  child: Row(
-                    children: [
-                      // cook time icon
-                      const Icon(
-                        Icons.schedule,
-                        color: Colors.yellow,
-                        size: 18,
-                      ),
-                      // sized box to separate icon and text
-                      const SizedBox(width: 7),
-                      // cook time text
-                      Text(
-                        cookTime,
-                        style: const TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
