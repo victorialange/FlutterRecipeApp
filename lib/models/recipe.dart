f97bbc4dc53c420ff3dcd8c768684ecdd5b111ef
victorialange <viclan2009@hotmail.com>
2023-04-12 05:56:30 -0400

    Change required data store in model from API

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -1,37 +1,33 @@
-// Recipe model that will hold data from API (Edamam)
+// recipe model that will hold the data from API
 class Recipe {
   // initialize variables
   final String label;
-  final String images;
-  final double calories;
-  final String totalTime;
+  final String image;
+  final String source;
+  final String url;
+  final List<String> ingredients;
 
-  const Recipe({
+  Recipe({
     required this.label,
-    required this.images,
-    required this.calories,
-    required this.totalTime,
+    required this.image,
+    required this.source,
+    required this.url,
+    required this.ingredients,
   });
 
   // define factory recipe from json function to instantiate recipe variables from json file retrieved from API
-  factory Recipe.fromJson(dynamic json) {
-    // return Recipe instance with parameters
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    List<String> ingredients = [];
+    for (var ingredient in json['ingredients']) {
+      ingredients.add(ingredient['text']);
+    }
+    // return recipe instance with values from json
     return Recipe(
-        // set name as string
-        label: json['label'] as String,
-        // set img as string
-        images: json['images']['THUMBNAIL']['url'] as String,
-        calories: json['calories'] as double,
-        totalTime: json['totalTime'] as String);
-  }
-
-  // define recipes from snapshot function to take a list of data that will be converted to a list of recipes
-  static List<Recipe> recipesFromShapshot(List snapshot) {
-    return snapshot.map((data) {
-      // return factory recipe.fromJson
-      return Recipe.fromJson(data);
-    })
-        // this will convert it back to a list
-        .toList();
+      label: json['label'],
+      image: json['image'],
+      source: json['source'],
+      url: json['url'],
+      ingredients: ingredients,
+    );
   }
 }
