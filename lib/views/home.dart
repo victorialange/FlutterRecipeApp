f5b1d4e15f922a1e6ff4d0ffbacf4e0f5cb42357
victorialange <viclan2009@hotmail.com>
2023-04-15 06:51:29 -0400

    Add placeholder data for more temporary rows for layout building purposes & initial styling

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -87,41 +87,254 @@ class _HomeScreenState extends State<HomeScreen> {
               }),
               // space between search bar and fetch results
               const SizedBox(height: 16),
+              // presenting the fetched data in vertical scroll view with multiple horizontally aligned lists of data
+              SingleChildScrollView(
+                // use column to display multiple rows of recipe data vertically (for now using same data for both rows, will be sorted by meal type based on different fetch methods with different parameters)
+                // have one list of results take up a limited amount of height for more rows
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  // children are the headings adding more context to fetched data, the data itself and boxes adding space between each child
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 7.5, right: 2.5),
+                      // have heading be displayed next to see more option above header
+                      child: Row(
+                        // separate heading and see all option
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        mainAxisSize: MainAxisSize.max,
+                        children: const [
+                          Text(
+                            "All recipes",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                          Text(
+                            "See all",
+                            style: TextStyle(
+                                color: Color(0xff0B9A61),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // sized box to contain horizontal scrollable recipe list with specified height
+                    SizedBox(
+                      height: 275,
+                      // LayoutBuilder widget to get the constraints of the parent widget and passing those constraints to ListView, so that it knows how much horizontal space available
+                      // wrap recipe card with fixed width of cardWidth (depending on width of the screen)
+                      child: LayoutBuilder(
+                        builder:
+                            (BuildContext context, BoxConstraints constraints) {
+                          return FutureBuilder(
+                            future: _recipes,
+                            builder:
+                                (BuildContext context, AsyncSnapshot snapshot) {
+                              if (snapshot.hasData) {
+                                // use snapshot data as List<Recipe> widget stored in recipes variable
+                                List<Recipe> recipes =
+                                    snapshot.data as List<Recipe>;
+                                // define cardWidth based on constraints
+                                double cardWidth = constraints.maxWidth / 1.5;
+                                // double cardWidth =
+                                //     MediaQuery.of(context).size.width * 0.7;
 
-                          // if there is more space available, increase card width
-                          if (constraints.maxWidth >= 600) {
-                            cardWidth = constraints.maxWidth / 3;
-                          }
-                          // return results in scrollable list view
-                          return ListView.builder(
-                            // display results in horizontal scrollable list
-                            scrollDirection: Axis.horizontal,
-                            itemCount: recipes.length,
-                            itemBuilder: (context, index) {
-                              // set size restrictions
-                              return SizedBox(
-                                // height: 50,
-                                width: cardWidth,
-                                // padding: const EdgeInsets.symmetric(horizontal: 8),
-                                child: RecipeCard(recipe: recipes[index]),
-                              );
+                                // if there is more space available, increase card width
+                                if (constraints.maxWidth >= 600) {
+                                  cardWidth = constraints.maxWidth / 3;
+                                }
+                                // return results in scrollable list view
+                                return ListView.builder(
+                                  // display results in horizontal scrollable list
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: recipes.length,
+                                  itemBuilder: (context, index) {
+                                    // set size restrictions
+                                    return SizedBox(
+                                      // height: 50,
+                                      width: cardWidth,
+                                      // padding: const EdgeInsets.symmetric(horizontal: 8),
+                                      child: RecipeCard(recipe: recipes[index]),
+                                    );
+                                  },
+                                );
+                              }
+                              // if there is an error display the specific error message
+                              else if (snapshot.hasError) {
+                                return Center(child: Text("${snapshot.error}"));
+                              }
+                              // if data hasn't been retreived yet (connectionState is pending/waiting, display progress indicator)
+                              return const Center(
+                                  child: CircularProgressIndicator());
                             },
                           );
-                        }
-                        // if there is an error display the specific error message
-                        else if (snapshot.hasError) {
-                          return Center(child: Text("${snapshot.error}"));
-                        }
-                        // if data hasn't been retreived yet (connectionState is pending/waiting, display progress indicator)
-                        return const Center(child: CircularProgressIndicator());
-                      },
-                    );
-                  },
+                        },
+                      ),
+                    ),
+                    // space between all recipes list and breakfast recipe heading
+                    const SizedBox(height: 16),
+                    // breakfast recipes heading
+                    Padding(
+                      padding: const EdgeInsets.only(left: 7.5, right: 2.5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        mainAxisSize: MainAxisSize.max,
+                        children: const [
+                          Text(
+                            "Breakfast recipes",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                          Text(
+                            "See all",
+                            style: TextStyle(
+                                color: Color(0xff0B9A61),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // breakfast recipes list
+                    SizedBox(
+                      height: 275,
+                      // LayoutBuilder widget to get the constraints of the parent widget and passing those constraints to ListView, so that it knows how much horizontal space available
+                      // wrap recipe card with fixed width of cardWidth (depending on width of the screen)
+                      child: LayoutBuilder(
+                        builder:
+                            (BuildContext context, BoxConstraints constraints) {
+                          return FutureBuilder(
+                            future: _recipes,
+                            builder:
+                                (BuildContext context, AsyncSnapshot snapshot) {
+                              if (snapshot.hasData) {
+                                // use snapshot data as List<Recipe> widget stored in recipes variable
+                                List<Recipe> recipes =
+                                    snapshot.data as List<Recipe>;
+                                // define cardWidth based on constraints
+                                double cardWidth = constraints.maxWidth / 1.5;
+                                // double cardWidth =
+                                //     MediaQuery.of(context).size.width * 0.7;
+
+                                // if there is more space available, increase card width
+                                if (constraints.maxWidth >= 600) {
+                                  cardWidth = constraints.maxWidth / 3;
+                                }
+                                // return results in scrollable list view
+                                return ListView.builder(
+                                  // display results in horizontal scrollable list
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: recipes.length,
+                                  itemBuilder: (context, index) {
+                                    // set size restrictions
+                                    return SizedBox(
+                                      // height: 50,
+                                      width: cardWidth,
+                                      // padding: const EdgeInsets.symmetric(horizontal: 8),
+                                      child: RecipeCard(recipe: recipes[index]),
+                                    );
+                                  },
+                                );
+                              }
+                              // if there is an error display the specific error message
+                              else if (snapshot.hasError) {
+                                return Center(child: Text("${snapshot.error}"));
+                              }
+                              // if data hasn't been retreived yet (connectionState is pending/waiting, display progress indicator)
+                              return const Center(
+                                  child: CircularProgressIndicator());
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                    // space between breakfast recipes list and lunch recipe heading
+                    const SizedBox(height: 16),
+                    // lunch recipes heading
+                    Padding(
+                      padding: const EdgeInsets.only(left: 7.5, right: 2.5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        mainAxisSize: MainAxisSize.max,
+                        children: const [
+                          Text(
+                            "Lunch recipes",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                          Text(
+                            "See all",
+                            style: TextStyle(
+                                color: Color(0xff0B9A61),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // lunch recipes list
+                    SizedBox(
+                      height: 275,
+                      // LayoutBuilder widget to get the constraints of the parent widget and passing those constraints to ListView, so that it knows how much horizontal space available
+                      // wrap recipe card with fixed width of cardWidth (depending on width of the screen)
+                      child: LayoutBuilder(
+                        builder:
+                            (BuildContext context, BoxConstraints constraints) {
+                          return FutureBuilder(
+                            future: _recipes,
+                            builder:
+                                (BuildContext context, AsyncSnapshot snapshot) {
+                              if (snapshot.hasData) {
+                                // use snapshot data as List<Recipe> widget stored in recipes variable
+                                List<Recipe> recipes =
+                                    snapshot.data as List<Recipe>;
+                                // define cardWidth based on constraints
+                                double cardWidth = constraints.maxWidth / 1.5;
+                                // double cardWidth =
+                                //     MediaQuery.of(context).size.width * 0.7;
+
+                                // if there is more space available, increase card width
+                                if (constraints.maxWidth >= 600) {
+                                  cardWidth = constraints.maxWidth / 3;
+                                }
+                                // return results in scrollable list view
+                                return ListView.builder(
+                                  // display results in horizontal scrollable list
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: recipes.length,
+                                  itemBuilder: (context, index) {
+                                    // set size restrictions
+                                    return SizedBox(
+                                      // height: 50,
+                                      width: cardWidth,
+                                      // padding: const EdgeInsets.symmetric(horizontal: 8),
+                                      child: RecipeCard(recipe: recipes[index]),
+                                    );
+                                  },
+                                );
+                              }
+                              // if there is an error display the specific error message
+                              else if (snapshot.hasError) {
+                                return Center(child: Text("${snapshot.error}"));
+                              }
+                              // if data hasn't been retreived yet (connectionState is pending/waiting, display progress indicator)
+                              return const Center(
+                                  child: CircularProgressIndicator());
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
+        ],
       ),
     );
   }
