16db50499cadec4bc409d8ac9f926eb2dc0ac191
victorialange <viclan2009@hotmail.com>
2023-04-16 15:31:08 -0400

    Define API call methods for breakfast, lunch & dinner recipes with different query params

--- lib/services/recipe_api.dart
+++ lib/services/recipe_api.dart
@@ -34,4 +34,70 @@ class RecipeApi {
       throw Exception('Failed to load recipes');
     }
   }
+
+  // getting breakfast recipes
+  Future<List<Recipe>> getBreakfastRecipes({required String query}) async {
+    // only target vegan recipes by specifying health parameter setting it to 'vegan'
+    final response = await http.get(Uri.parse(
+        '$_baseUrl?type=public&q=$query&app_id=$_appId&app_key=$_appKey&health=vegan&mealType=Breakfast&tag=vegan'));
+
+    // checking if response status code ok
+    if (response.statusCode == 200) {
+      // decoding the response
+      final data = jsonDecode(response.body);
+      // fill instance of recipe model with fetched data
+      final breakfastRecipes = List<Recipe>.from(
+          // all the relevant data is inside the hits list under the recipe map in the response json object
+          data['hits'].map((hit) => Recipe.fromJson(hit['recipe'])));
+      return breakfastRecipes;
+    }
+    // handle fetch error
+    else {
+      throw Exception('Failed to load breakfast recipes');
+    }
+  }
+
+  // getting lunch recipes
+  Future<List<Recipe>> getLunchRecipes({required String query}) async {
+    // only target vegan recipes by specifying health parameter setting it to 'vegan'
+    final response = await http.get(Uri.parse(
+        '$_baseUrl?type=public&q=$query&app_id=$_appId&app_key=$_appKey&health=vegan&mealType=Lunch&dishType=Main%20course&dishType=Salad&tag=vegan'));
+
+    // checking if response status code ok
+    if (response.statusCode == 200) {
+      // decoding the response
+      final data = jsonDecode(response.body);
+      // fill instance of recipe model with fetched data
+      final lunchRecipes = List<Recipe>.from(
+          // all the relevant data is inside the hits list under the recipe map in the response json object
+          data['hits'].map((hit) => Recipe.fromJson(hit['recipe'])));
+      return lunchRecipes;
+    }
+    // handle fetch error
+    else {
+      throw Exception('Failed to load lunch recipes');
+    }
+  }
+
+  // getting lunch recipes
+  Future<List<Recipe>> getDinnerRecipes({required String query}) async {
+    // only target vegan recipes by specifying health parameter setting it to 'vegan'
+    final response = await http.get(Uri.parse(
+        '$_baseUrl?type=public&q=$query&app_id=$_appId&app_key=$_appKey&health=vegan&mealType=Dinner&dishType=Main%20course&dishType=Souo&tag=vegan'));
+
+    // checking if response status code ok
+    if (response.statusCode == 200) {
+      // decoding the response
+      final data = jsonDecode(response.body);
+      // fill instance of recipe model with fetched data
+      final dinnerRecipes = List<Recipe>.from(
+          // all the relevant data is inside the hits list under the recipe map in the response json object
+          data['hits'].map((hit) => Recipe.fromJson(hit['recipe'])));
+      return dinnerRecipes;
+    }
+    // handle fetch error
+    else {
+      throw Exception('Failed to load dinner recipes');
+    }
+  }
 }
