6f05e3904155e859dfc95978fb7ca4746a7a41a3
victorialange <viclan2009@hotmail.com>
2023-04-19 06:40:58 -0400

    Build login screen and sign up screen

---
+++ lib/login_screen.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:recipe_app/services/firebase_signin.dart';
+import 'package:recipe_app/views/home.dart';
+// import 'package:neumorphic_button/neumorphic_button.dart';
+// import 'package:recipe_app/services/initiate_firebase_auth.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.network(
+            'https://images.squarespace-cdn.com/content/v1/562d08fee4b072888c2d4110/1538001622543-EI6C4BZ1ZBD2047ZFFYV/katherine-chase-518230-unsplash.jpg?format=1500w',
+            height: MediaQuery.of(context).size.height,
+          ),
+          Padding(
+            padding:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
+            child: SafeArea(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Sign In",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineMedium
+                          ?.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      children: [
+                        const Text("Don't have an account yet?"),
+                        TextButton(
+                          onPressed: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const RegistrationForm())),
+                          child: const Text(
+                            "Sign Up",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16.0 * 2),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextFormField(
+                            controller: _emailController,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: const InputDecoration(
+                                labelText: 'Email',
+                                hintText: 'Your email',
+                                icon: Icon(Icons.email)),
+                          ),
+                          const SizedBox(height: 16.0),
+                          TextFormField(
+                            controller: _emailController,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: const InputDecoration(
+                                labelText: 'Password',
+                                hintText: '******',
+                                icon: Icon(Icons.lock)),
+                            obscureText: true,
+                          ),
+                          const SizedBox(height: 16.0),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 16.0 * 2),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          try {
+                            final UserCredential userCredential =
+                                await FirebaseAuth.instance
+                                    .signInWithEmailAndPassword(
+                              email: _emailController.text.trim(),
+                              password: _passwordController.text.trim(),
+                            );
+                            print(
+                                "User logged in: ${userCredential.user!.uid}");
+
+                            // Navigate to HomePage after successful login
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const HomeScreen()),
+                            );
+                          } catch (e) {
+                            if (e is FirebaseAuthException) {
+                              if (e.code == 'user-not-found') {
+                                print('No user found for that email.');
+                              } else if (e.code == 'wrong-password') {
+                                print('Wrong password provided for that user.');
+                              } else {
+                                print('Error: ${e.message}');
+                              }
+                            } else {
+                              print('Error: $e');
+                            }
+                          }
+                        },
+                        child: const Text("Sign In"),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Future<void> _signInUser() async {
+  //   try {
+  //     final UserCredential userCredential =
+  //         await FirebaseAuth.instance.signInWithEmailAndPassword(
+  //       email: _emailController.text.trim(),
+  //       password: _passwordController.text.trim(),
+  //     );
+  //     print("User logged in: ${userCredential.user!.uid}");
+
+  //     // Navigate to HomePage after successful login
+  //     Navigator.push(
+  //       context,
+  //       MaterialPageRoute(builder: (context) => const HomeScreen()),
+  //     );
+  //   } catch (e) {
+  //     if (e is FirebaseAuthException) {
+  //       if (e.code == 'user-not-found') {
+  //         print('No user found for that email.');
+  //       } else if (e.code == 'wrong-password') {
+  //         print('Wrong password provided for that user.');
+  //       } else {
+  //         print('Error: ${e.message}');
+  //       }
+  //     } else {
+  //       print('Error: $e');
+  //     }
+  //   }
+  // }
+}
---
+++ lib/services/firebase_signin.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '/login_screen.dart';
+import 'package:recipe_app/views/home.dart';
+
+class RegistrationForm extends StatefulWidget {
+  const RegistrationForm({Key? key}) : super(key: key);
+
+  @override
+  _RegistrationFormState createState() => _RegistrationFormState();
+}
+
+class _RegistrationFormState extends State<RegistrationForm> {
+  // create a global key for the form
+  final _formKey = GlobalKey<FormState>();
+  // create a text editing controller for the email input
+  final _emailController = TextEditingController();
+  // create a text editing controller for the password input
+  final _passwordController = TextEditingController();
+  // create a controller for username
+  final _nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      // add padding around the Form widget
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.network(
+            'https://images.squarespace-cdn.com/content/v1/562d08fee4b072888c2d4110/1538001622543-EI6C4BZ1ZBD2047ZFFYV/katherine-chase-518230-unsplash.jpg?format=1500w',
+            height: MediaQuery.of(context).size.height,
+          ),
+          Center(
+            child: SafeArea(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Create Account",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineSmall!
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      children: [
+                        const Text("Already have an account?"),
+                        TextButton(
+                          onPressed: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => LoginScreen(
+                                  key: _formKey,
+                                ),
+                              )),
+                          child: const Text(
+                            "Sign In!",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16.0 * 2),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextFormField(
+                            controller: _nameController,
+                            decoration: const InputDecoration(
+                              hintText: "username",
+                              labelText: "Username",
+                            ),
+                            validator: (username) {
+                              if (username == null || username.isEmpty) {
+                                return 'Username is required';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 16.0),
+                          // email text field
+                          TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            controller: _emailController,
+                            decoration: const InputDecoration(
+                              hintText: "Your email",
+                              labelText: "Email",
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please enter your email';
+                              } else if (!value.contains('@')) {
+                                return 'Please enter a valid email';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 16.0),
+                          // password field
+                          TextFormField(
+                            // We want to hide our password
+                            controller: _passwordController,
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              hintText: "******",
+                              labelText: "Password",
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please enter your password';
+                              } else if (value.length < 6) {
+                                return 'Password should be at least 6 characters long';
+                              }
+                              return null;
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 16.0 * 2),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            // Sign up form is done
+                            // It saved our inputs
+                            _registerUser();
+                            _formKey.currentState!.save();
+                          }
+                        },
+                        child: const Text("Sign Up"),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // register user method
+  Future<void> _registerUser() async {
+    try {
+      final UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: _emailController.text, password: _passwordController.text);
+      print("User registered: ${userCredential.user!.uid}");
+      // Navigate to HomePage after successful login
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    } on FirebaseAuthException catch (e) {
+      print("Error registering user: ${e.code}");
+    } catch (e) {
+      print("Error registering user: $e");
+    }
+  }
+}
